9d080aa2738f04ab464aef3c06d4c687aa88455f
Egor2001 <elchinov.es@gmail.com>
2020-10-25 03:21:39 +0300

    main refactored

--- src/plane/main.cpp
+++ src/plane/main.cpp
@@ -1,4 +1,4 @@
-#define WM_BENCHMARK
+// #define WM_BENCHMARK
 
 #include "solver2d.h"
 #include "general_solver2d.h"
@@ -19,66 +19,43 @@
 
 using namespace wave_model;
 
-template<typename TStream>
-TStream& test(TStream& stream);
-
-void benchmark();
-void test_wave(int argc, char* argv[]);
-
-int main([[maybe_unused]] int argc, 
-         [[maybe_unused]] char* argv[])
+template<size_t NSideRank, size_t NTileRank = NSideRank - 2>
+auto run_scalar(double length /* = 1e2 */, 
+                double delta_time /* = 0.1 */, 
+                size_t run_count /* = 1 << ((15 - NSideRank) * 2) */)
 {
-#if defined(WM_BENCHMARK)
-    benchmark();
-#else
-    test_wave(argc, argv);
-#endif
+    static_assert(NTileRank < NSideRank, "tile must be less than side");
 
-    return 0;
-}
-
-template<typename TStream>
-TStream& test(TStream& stream)
-{
-    wm_test_linear_layer2d(stream);
-    wm_test_zcurve_layer2d(stream);
-
-    wm_test_conefold_tiling2d<WmLinearLayer2D>(stream);
-    wm_test_conefold_tiling2d<WmZCurveLayer2D>(stream);
-
-    return stream;
-}
-
-void benchmark()
-{
-    static constexpr size_t NTileRank = 3;
-    static constexpr size_t NSideRank = 7;
-
-    static constexpr size_t NRunCount = 
-        1ull << ((15 - NSideRank) * 2);
-
-    //WmCosineHatWave2D init_wave { /* .ampl = */ 1.0, /* .freq = */ 0.5 };
-    /*
+    WmCosineHatWave2D init_wave { /* .ampl = */ 1.0, /* .freq = */ 0.5 };
     auto init_func = [&init_wave](double x, double y) -> WmBasicWaveData2D
     { 
         return { 
-            // .factor =
-            1.0,
-            // .intencity =
-            init_wave(x, y) 
+            /* .factor = */ 1.0,
+            /* .intencity = */ init_wave(x, y) 
         }; 
     };
 
     WmSolver2D<WmBasicWaveStencil2D, 
                WmConeFoldTiling2D<NTileRank>, 
                WmLinearLayer2D, NSideRank> 
-        solver(1e2, 0.1, init_func);
-    */
+        solver(length, delta_time, init_func);
 
-    static constexpr double length = 1e2;
-    static constexpr double delta = length / (1u << (NSideRank - 2));
+    solver.advance(run_count);
+
+    return solver;
+}
+
+template<size_t NSideRank, size_t NTileRank = NSideRank - 2>
+auto run_vector(double length /* = 1e2 */, 
+                double delta_time /* =  0/1 */, 
+                size_t run_count /* = 1 << ((15 - NSideRank) * 2) */)
+{
+    static_assert(NTileRank < NSideRank, "tile must be less than side");
+
+    double delta = length / (1u << (NSideRank - 2));
     WmCosineHatWave2D init_wave { /* .ampl = */ 1.0, /* .freq = */ 0.5 };
-    auto init_func = [&init_wave](double x, double y) -> WmAvxBasicWaveData2D
+    auto init_func = [&init_wave, delta](double x, double y) -> 
+        WmAvxBasicWaveData2D
     { 
         return { 
             // .factor = 
@@ -95,39 +72,28 @@ void benchmark()
     WmGeneralSolver2D<WmAvxBasicWaveStencil2D, 
                       WmGeneralConeFoldTiling2D<NTileRank>, 
                       WmAvxLinearLayer2D, NSideRank - 2> 
-        solver(length, 0.1, init_func);
+        solver(length, delta_time, init_func);
 
-    solver.advance(NRunCount);
+    solver.advance(run_count);
+
+    return solver;
 }
 
-void test_wave(int argc, char* argv[])
+template<typename TStream>
+TStream& test(TStream& stream)
 {
-    static constexpr size_t NTileRank = 3;
-    static constexpr size_t NSideRank = 5;
-    static constexpr size_t NRunCount = 100; 
+    wm_test_linear_layer2d(stream);
+    wm_test_zcurve_layer2d(stream);
 
-    static constexpr double length = 1e2;
-    static constexpr double delta = length / (1u << NSideRank);
-    WmCosineHatWave2D init_wave { /* .ampl = */ 1.0, /* .freq = */ 0.5 };
-    auto init_func = [&init_wave](double x, double y) -> WmAvxBasicWaveData2D
-    { 
-        return { 
-            // .factor = 
-                _mm256_set1_pd(1.0),
-            // .intencity = 
-            // TODO: to replace delta / 4 with more convenient interface
-                _mm256_setr_pd(init_wave(4.0 * x + 0.00 * delta, y), 
-                               init_wave(4.0 * x + 0.25 * delta, y), 
-                               init_wave(4.0 * x + 0.50 * delta, y), 
-                               init_wave(4.0 * x + 0.75 * delta, y)) 
-        }; 
-    };
+    wm_test_conefold_tiling2d<WmLinearLayer2D>(stream);
+    wm_test_conefold_tiling2d<WmZCurveLayer2D>(stream);
 
-    WmGeneralSolver2D<WmAvxBasicWaveStencil2D, 
-               WmGeneralConeFoldTiling2D<NTileRank>, 
-               WmAvxLinearLayer2D, NSideRank> 
-        solver(length, 0.1, init_func);
+    return stream;
+}
 
+template<size_t NSideRank, size_t NTileRank = NSideRank - 2>
+void test_wave(int argc, char* argv[], size_t run_count)
+{
     if (argc < 2)
         std::cerr << "USAGE: " << argv[0] << " OUTFILE [LOGFILE]\n";
 
@@ -138,6 +104,17 @@ void test_wave(int argc, char* argv[])
         test(WmLogger::stream());
     }
 
-    solver.advance(NRunCount);
+    auto solver = run_vector<NSideRank, NTileRank>(1e2, 0.1, run_count);
     solver.layer().dump(out_stream);
+}
+
+int main(int argc, char* argv[])
+{
+#if defined(WM_BENCHMARK)
+    run_scalar<7>(1e2, 0.1, 50);
+#else
+    test_wave<5>(argc, argv, 100);
+#endif
+
+    return 0;
 }
