3bd2d1d9ad43c3e42be0121bafb975223bf4fa32
Egor2001 <elchinov.es@gmail.com>
2020-11-23 16:58:08 +0300

    avx benchmark

--- result/benchviz.py
+++ result/benchviz.py
@@ -5,12 +5,12 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
+    if len(sys.argv) != 5:
         print('usage: ', sys.argv[0], ' DATA_1_CSV DATA_2_CSV OUTPUT_PNG\n')
         sys.exit()
 
-    name_1, name_2 = str(sys.argv[1]), str(sys.argv[2])
-    bench_1, bench_2 = pd.read_csv(name_1), pd.read_csv(name_2)
+    name_1, name_2, name_3 = str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3])
+    bench_1, bench_2, bench_3 = pd.read_csv(name_1), pd.read_csv(name_2), pd.read_csv(name_3)
 
     fig, ax = plt.subplots()
     ax.set_title('time benchmark [~1 billion stencil computations]')
@@ -19,8 +19,10 @@ if __name__ == "__main__":
     ax.plot(bench_1.Grid, bench_1.Time, 
             color='red', label=name_1.split(".")[0])
     ax.plot(bench_2.Grid, bench_2.Time, 
-            color='black', label=name_2.split(".")[0])
+            color='blue', label=name_2.split(".")[0])
+    ax.plot(bench_3.Grid, bench_3.Time, 
+            color='black', label=name_3.split(".")[0])
     ax.legend()
 
-    plt.savefig(str(sys.argv[3]))
+    plt.savefig(str(sys.argv[4]))
 
