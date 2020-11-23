b8e2f75a6e6293a75512c7f8ae44cafa53ed3494
Egor2001 <elchinov.es@gmail.com>
2020-11-23 03:46:09 +0300

    diamondtorre in progress

--- src/plane/tiling/general_diamondtorre_tiling2d_loop.h
+++ src/plane/tiling/general_diamondtorre_tiling2d_loop.h
@@ -33,6 +33,11 @@ struct WmGeneralDiamondTorreTiling2D
 
     static_assert(NTileRank > 0, "NTileRank must be positive");
 
+    enum ELine
+    {
+        LINE_R, LINE_L
+    };
+
     enum EType
     {
         TYPE_A, TYPE_B, TYPE_C, TYPE_D, TYPE_N
@@ -40,13 +45,9 @@ struct WmGeneralDiamondTorreTiling2D
 
     // TODO: to generate code for the each case
     template<size_t NRank, typename TStencil, typename TGeneralLayer>
-    static void traverse(const TStencil& stencil, 
-                         TGeneralLayer* layers) noexcept
+    static void traverse(const TStencil& stencil, TGeneralLayer* layers) 
+                         noexcept
     {
-        // guaranteed to be 2's power
-        static constexpr size_t NLineCnt = 
-            TGeneralLayer::NDomainLengthX / (1u << NTileRank);
-
         int64_t left_idx = 
             TGeneralLayer::template 
             off_right<TGeneralLayer::NDomainRankX>(0, 1) + 
@@ -57,122 +58,138 @@ struct WmGeneralDiamondTorreTiling2D
             TGeneralLayer::template off_right<NTileRank - 1>(left_idx, 1) +
             TGeneralLayer::template off_top<NTileRank - 1>(left_idx, 1);
 
-        proc_line<NRank, true>(right_idx, 0, TGeneralLayer::NDomainLengthX + 
-                NTileSize / 2, stencil, layers);
-        proc_line<NRank, false>(left_idx, 0, TGeneralLayer::NDomainLengthX, 
-                stencil, layers);
-        for (int64_t col = TGeneralLayer::NDomainLengthX - NTileSize; 
-             col >= 0; col -= NTileSize)
+        int64_t col = TGeneralLayer::NDomainLengthX;
+        proc_line<NRank, 1 + NTileSize / 2, LINE_R>
+            (col + NTileSize / 2, right_idx, 0, stencil, layers);
+        proc_line<NRank, 1, LINE_L>
+            (col, left_idx, 0, stencil, layers);
+
+        for (col = col - NTileSize; col >= NTileSize; col -= NTileSize)
         {
             right_idx += TGeneralLayer::template 
                 off_left<NTileRank>(right_idx, 1);
             left_idx += TGeneralLayer::template 
                 off_left<NTileRank>(left_idx, 1);
 
-            proc_line<NRank, true>(right_idx, 0, col + 
-                    NTileSize / 2, stencil, layers);
-            proc_line<NRank, false>(left_idx, 0, col, 
-                    stencil, layers);
+            proc_line<NRank, 0, LINE_R>
+                (col + NTileSize / 2, right_idx, 0, stencil, layers);
+            proc_line<NRank, 0, LINE_L>
+                (col, left_idx, 0, stencil, layers);
         }
 
-        right_idx += TGeneralLayer::template off_left<NTileRank>(right_idx, 1);
-        left_idx += TGeneralLayer::template off_left<NTileRank>(left_idx, 1);
+        right_idx += TGeneralLayer::template 
+            off_left<NTileRank>(right_idx, 1);
+        left_idx += TGeneralLayer::template 
+            off_left<NTileRank>(left_idx, 1);
 
-        proc_line<NRank, false>(left_idx, 0, 
-                NTileSize / 2, stencil, layers);
-        proc_line<NRank, true>(right_idx, 0, 
-                0, stencil, layers);
+        proc_line<NRank, 1 - NTileSize / 2, LINE_R>
+            (col + NTileSize / 2, right_idx, 0, stencil, layers);
+        proc_line<NRank, 1 - NTileSize, LINE_L>
+            (col, left_idx, 0, stencil, layers);
 
         for (size_t cur_time = 0; cur_time < (1u << NRank);)
         {
             cur_time += NTileSize / 2;
-            proc_line<NRank, true>(right_idx, cur_time % TStencil::NMod, 
-                    NTileSize / 2, stencil, layers);
+            proc_line<NRank, 1 - NTileSize, LINE_R>
+                (0, left_idx, cur_time, stencil, layers);
 
             cur_time += NTileSize / 2;
-            proc_line<NRank, false>(left_idx, cur_time % TStencil::NMod, 
-                    0, stencil, layers);
+            proc_line<NRank, 1 - NTileSize, LINE_L>
+                (0, left_idx, cur_time, stencil, layers);
         }
     }
 
-    template<size_t NRank, bool NIsWide, 
+    template<size_t NRank, int NOffset, ELine NType, 
              typename TStencil, typename TGeneralLayer>
-    static void proc_line(int64_t idx, int64_t layer_idx, int64_t coord, 
-            const TStencil& stencil, TGeneralLayer* layers) noexcept
+    static void proc_line(int64_t coord, int64_t idx, int64_t layer_idx, 
+                          const TStencil& stencil, TGeneralLayer* layers) 
+                          noexcept
     {
-        if constexpr (NType == TYPE_N)
-            return;
-
         WM_ASSERT(idx >= 0, "index must be non-negative");
 
-        int64_t max_row = (NIsWide ? 
-                TGeneralLayer::NDomainLengthY / (1u << NTileRank) + 1 : 
-                TGeneralLayer::NDomainLengthY / (1u << NTileRank));
-
-        static constexpr size_t NTypeTop = []() -> size_t {
-            if constexpr (NIsWide) return TYPE_A;
-            else return TYPE_D;
-        }();
-
-        static constexpr size_t NTypeBottom = []() -> size_t {
-            if constexpr (NIsWide) return TYPE_B;
-            else return TYPE_C;
-        }();
-
-        proc_tile<NRank, NTypeBottom>(idx, layer_idx, coord, stencil, layers);
-        for (int64_t row = 1; row < max_row; ++row)
+        if constexpr (NType == LINE_R)
         {
-            idx += TGeneralLayer::template off_top<NTileRank>(idx, 1);
-            proc_tile<NRank, TYPE_B>(idx, layer_idx, coord, stencil, layers);
+            proc_pole<NRank, NOffset, TYPE_C>
+                (coord, idx, layer_idx, stencil, layers);
+        }
+        else if constexpr (NType == LINE_L)
+        {
+            proc_pole<NRank, NOffset, TYPE_D>
+                (coord, idx, layer_idx, stencil, layers);
         }
 
-        idx += TGeneralLayer::template off_top<NTileRank>(idx, 1);
-        proc_tile<NRank, NTypeTop>(idx, layer_idx, coord, stencil, layers);
-    }
-
-    template<size_t NRank, EType NType, 
-             typename TStencil, typename TGeneralLayer>
-    proc_tile(int64_t idx, int64_t layer_idx, int64_t coord, 
-            const TStencil& stencil, TGeneralLayer* layers) noexcept
-    {
-        for (; coord < TGeneralLayer::NDomainLengthX + NTileSize - 1; ++coord)
+        for (int64_t row = TGeneralLayer::NDomainLengthY - NTileSize; 
+             row > 0; row -= NTileSize)
         {
-            proc_fold<NTileRank, NType>(idx, layer_idx, coord, stencil, layers);
-            layer_idx = (layer_idx + 1) % TStencil::NMod;
+            idx += TGeneralLayer::template 
+                off_top<NTileRank>(right_idx, 1);
+
+            proc_pole<NRank, NOffset, TYPE_B>
+                (coord, idx, layer_idx, stencil, layers);
+        }
+
+        if constexpr (NType == LINE_L)
+        {
+            idx += TGeneralLayer::template 
+                off_top<NTileRank>(right_idx, 1);
+
+            proc_pole<NRank, NOffset, TYPE_A>
+                (coord, idx, layer_idx, stencil, layers);
         }
     }
 
-    template<size_t NCurRank, EType NType, 
+    template<size_t NRank, int NOffset, EType NType, 
              typename TStencil, typename TGeneralLayer>
-    proc_fold(int64_t idx, int64_t layer_idx, int64_t coord, 
-            const TStencil& stencil, TGeneralLayer* layers) noexcept
+    proc_pole(int64_t coord, int64_t idx, int64_t layer_idx, 
+              const TStencil& stencil, TGeneralLayer* layers) noexcept
     {
-        if constexpr (NCurRank == 1)
+        proc_fold<NTileRank, NOffset, NType>
+            (idx, layer_idx, stencil, layers);
+
+        layer_idx += NTileSize;
+
+        int64_t max_coord = 
+            std::min(TGeneralLayer::NDomainLengthX, (1 << NRank) - layer_idx);
+
+        coord += NTileSize;
+        for (; coord < max_coord; coord += NTileSize)
+        {
+            idx += TGeneralLayer::template 
+                off_right<NTileRank>(right_idx, 1);
+
+            proc_fold<NTileRank, 0, NType>
+                (idx, layer_idx, stencil, layers);
+
+            layer_idx += NTileSize;
+        }
+
+        if (coord >= TGeneralLayer::NDomainLengthX)
+        {
+            idx += TGeneralLayer::template 
+                off_right<NTileRank>(right_idx, 1);
+
+            proc_fold<NTileRank, NTileSize + NOffset, NType>
+                (idx, layer_idx, stencil, layers);
+        }
+    }
+
+    template<size_t NRank, int NOffset, EType NType, 
+             typename TStencil, typename TGeneralLayer>
+    proc_fold(int64_t idx, int64_t layer_idx, 
+              const TStencil& stencil, TGeneralLayer* layers) noexcept
+    {
+        if constexpr (NRank == 1)
         {
             if constexpr (NType == TYPE_D || NType == TYPE_N)
                 return;
 
-            switch (coord)
-            {
-                case 0:
-                    calc_cell<>
-                    break;
-                case 1:
-                    calc_cell<>
-                    calc_cell<>
-                    break;
-                case TGeneralLayer::NDomainLengthX - 1:
-                    calc_cell<>
-                    calc_cell<>
-                    break;
-                case TGeneralLayer::NDomainLengthX:
-                    calc_cell<>
-                    break;
-                default:
-                    calc_cell<>
-                    calc_cell<>
-                    break;
-            }
+            constexpr auto FCaller = [](size_t rest) {
+                if (layer_idx % TStencil::NMod == rest)
+                    calc_cell<>();
+            };
+
+            if constexpr (NOffset ... && NType ...)
+                calc_cell<NType>
         }
         else
         {
